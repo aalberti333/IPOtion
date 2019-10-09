b906a08658560eb93f40b07965b4d09b41356659
aalberti333 <aalberti333@protonmail.com>
2019-10-08 18:55:01 -0600

    sort maps by datetime

--- lib/ip_otion.ex
+++ lib/ip_otion.ex
@@ -16,13 +16,21 @@ defmodule IPOtion do
 
   """
 
-  def ticker(tick) do
+  def fetch(ticker) do
     url_start = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
     url_end = "&outputsize=full&apikey=" <> System.get_env("ALPHA_KEY")
 
-    {:ok, response} = Mojito.request(method: :get, url: url_start <> tick <> url_end)
+    {:ok, response} = Mojito.request(method: :get, url: url_start <> ticker <> url_end)
 
-    ticker_map = Jason.decode(response.body)
-    IO.inspect(ticker_map)
+    {:ok, ticker_map} = Jason.decode(response.body)
+
+    # TO DO: Add a Enum.map here to generalize the first two elements (MetaData and Time Series (Daily))
+    ticker_map["Time Series (Daily)"]
   end
+
+  def datestr_to_datetime(ticker_map) do
+    clean_date = Enum.map(ticker_map, fn {k, v} -> {Date.from_iso8601!(k), v} end)
+    sorted_date = Enum.sort_by(clean_date, fn {d, v} -> {{d.year, d.month, d.day}, v} end)
+  end
+
 end
