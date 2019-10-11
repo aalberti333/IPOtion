41759d29a7f5b5b103ca4eb0aa02b6d742c4b8f0
aalberti333 <aalberti333@protonmail.com>
2019-10-11 09:08:31 -0600

    to dos

--- lib/ip_otion.ex
+++ lib/ip_otion.ex
@@ -16,12 +16,15 @@ defmodule IPOtion do
 
   """
 
+  # TO DO: Map api to selection.
+  def api_select()
+
+  # TO DO: Provide default api if api_select() was not previously called
   def fetch(ticker) do
     url_start = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol="
     url_end = "&outputsize=full&apikey=" <> System.get_env("ALPHA_KEY")
 
     {:ok, response} = Mojito.request(method: :get, url: url_start <> ticker <> url_end)
-
     {:ok, ticker_map} = Jason.decode(response.body)
 
     # TO DO: Add a Enum.map here to generalize the first two elements (MetaData and Time Series (Daily))
