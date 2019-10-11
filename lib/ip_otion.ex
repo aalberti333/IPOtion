c7203d55efc5f2889aa7a58f05f807f1439f5f3b
aalberti333 <aalberti333@protonmail.com>
2019-10-11 11:33:06 -0600

    change naming convention, switch from cond to case

--- lib/ip_otion.ex
+++ lib/ip_otion.ex
@@ -7,20 +7,20 @@ defmodule IPOtion do
   """
 
   @doc """
-  Select API functionality to use to retrieve financial data
+  Select API series_type to use to retrieve financial data
 
   ## Examples
       iex> IPOtion.api_select "Alpha Vantage"
       {:ok, <start_of_api_url_here>, <end_of_api_url_here>}
 
   """
-  def api_functionality(functionality) do
+  def api_series_type(series_type) do
     # TO DO: add more functionalities
-    cond do
-      functionality == "TIME_SERIES_DAILY" ->
+    case series_type do
+      "TIME_SERIES_DAILY" ->
         {:ok, url_start, url_end} = {:ok, "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=", "&outputsize=full&apikey=" <> System.get_env("ALPHA_KEY")}
-      true ->
-        raise "You must select a valid api functionality. Available choices are: \"TIME_SERIES_DAILY\", ..."
+      _ ->
+        raise "You must select a valid api series_type. Available choices are: \"TIME_SERIES_DAILY\", ..."
     end
   end
 
@@ -29,20 +29,18 @@ defmodule IPOtion do
 
   ## Examples
 
-      iex> IPOtion.fetch("MSFT", url_start, url_end, functionality)
+      iex> IPOtion.fetch("MSFT", url_start, url_end, series_type)
       %Mojito.Response{ body: "{<market-data-here>}" }
 
   """
-  def fetch(ticker, url_start, url_end, functionality) do
+  def fetch(ticker, url_start, url_end, series_type) do
     {:ok, response} = Mojito.request(method: :get, url: url_start <> ticker <> url_end)
     {:ok, ticker_map} = Jason.decode(response.body)
 
     # TO DO: add more functionalities
-    cond do
-      functionality == "TIME_SERIES_DAILY" ->
-        ticker_map["Time Series (Daily)"]
-      true ->
-        raise "You must select a valid api functionality. Available choices are: \"TIME_SERIES_DAILY\", ..."
+    case series_type do
+      "TIME_SERIES_DAILY" -> ticker_map["Time Series (Daily)"]
+      _ -> raise "You must select a valid api series_type. Available choices are: \"TIME_SERIES_DAILY\", ..."
     end
   end
 
